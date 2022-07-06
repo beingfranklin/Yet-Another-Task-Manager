6a38c124f9ec14fb5d610bd0fcca0ec2406d4e1a
Franklin Antony <franklinanton.007@gmail.com>
2022-07-06 14:32:16 +0100

    Added Pipe Validations using class validator and class transformer

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,7 +1,12 @@
+import { IsNotEmpty, IsOptional } from 'class-validator';
 import { TaskStatus } from '../tasks.model';
 
 export class CreateTaskDto {
+  @IsNotEmpty()
   title: string;
+  @IsNotEmpty()
   description: string;
+  @IsOptional()
+  @IsNotEmpty()
   status?: TaskStatus;
 }
--- src/tasks/dto/get-tasks.dto.ts
+++ src/tasks/dto/get-tasks.dto.ts
@@ -1,6 +1,9 @@
+import { IsOptional } from 'class-validator';
 import { TaskStatus } from '../tasks.model';
 
 export class GetTaskFilterDto {
+  @IsOptional()
   status?: TaskStatus;
+  @IsOptional()
   search?: string;
 }
